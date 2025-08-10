49fbc1bbbfa0ddf555ba29a8cfb15010e008814f
Henrickqt <pedro.msg@hotmail.com>
2025-08-10 11:45:39 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
-import Header from "@/components/shared/header";
+import Image from "next/image";
 
-export default function Home() {
+import Header from "@/components/shared/header";
+import ProductList from "@/components/shared/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src={"/banner-01.png"}
+            alt="Leve uma vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+
+        <ProductList title="Produtos em destaque" products={products} />
+
+        <div className="px-5">
+          <Image
+            src={"/banner-02.png"}
+            alt="Seja autêntico"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
     </>
   );
 }
+
+export default Home;
